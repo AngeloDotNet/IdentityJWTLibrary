e3f38ee8e51d769d4784efed284929809586022e
Angelo Pirola <angelo9871@gmail.com>
2022-09-25 20:09:08 +0200

    Aggiunto BusinessLayer Settings

---
+++ src/IdentityAuth/BusinessLayer/Settings/JwtSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace IdentityAuth.BusinessLayer.Settings;
+
+public class JwtSettings
+{
+    public string SecurityKey { get; init; }
+    public string Issuer { get; init; }
+    public string Audience { get; init; }
+    public int AccessTokenExpirationMinutes { get; init; }
+    public int RefreshTokenExpirationMinutes { get; init; }
+}
