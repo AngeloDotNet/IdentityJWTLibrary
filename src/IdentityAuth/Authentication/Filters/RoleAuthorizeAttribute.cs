da58b76a9ba39bb7084931cd48e326c43e143b5c
Angelo Pirola <angelo9871@gmail.com>
2022-09-25 20:10:11 +0200

    Aggiunta sezione Authentication Filters

---
+++ src/IdentityAuth/Authentication/Filters/RoleAuthorizeAttribute.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace IdentityAuth.Authentication.Filters;
+
+public class RoleAuthorizeAttribute : AuthorizeAttribute
+{
+    public RoleAuthorizeAttribute(params string[] roles)
+    {
+        Roles = string.Join(",", roles);
+    }
+}
