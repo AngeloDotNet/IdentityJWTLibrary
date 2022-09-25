2ac7bbbcb38c2cf922a69fdc94514293b794c242
Angelo Pirola <angelo9871@gmail.com>
2022-09-25 20:09:57 +0200

    Aggiunta sezione Authentication Entities

---
+++ src/IdentityAuth/Authentication/Entities/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IdentityAuth.Authentication.Entities;
+
+public class ApplicationUser : IdentityUser<Guid>
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string RefreshToken { get; set; }
+
+    public DateTime? RefreshTokenExpirationDate { get; set; }
+
+    public virtual ICollection<ApplicationUserRole> UserRoles { get; set; }
+}
---
+++ src/IdentityAuth/Authentication/Entities/ApplicationUserRole.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IdentityAuth.Authentication.Entities;
+
+public class ApplicationUserRole : IdentityUserRole<Guid>
+{
+    public virtual ApplicationUser User { get; set; }
+
+    public virtual ApplicationRole Role { get; set; }
+}
